d23a495cbc4f0af3f7056f055be4b1d2041b4a39
soroush <soroushfalaki24@hotmail.com>
2026-02-03 21:37:39 +0330

    fix index

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import connectDB from './configs/dbConfig.js';
 import limiter from './utils/limiter.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
 import express from 'express';
 const app = express();
 
+//docgen
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
